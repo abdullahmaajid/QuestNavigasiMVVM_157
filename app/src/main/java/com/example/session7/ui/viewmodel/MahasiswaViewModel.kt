c5b200d3db3edfd0ad69c40fc34508f9ec298831
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-11-13 16:11:22 +0700

    di MahasiswaViewModel menambahkan function untuk save data mahasiswa yang nanti untuk wadah datanya

--- app/src/main/java/com/example/session7/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/session7/ui/viewmodel/MahasiswaViewModel.kt
@@ -10,4 +10,18 @@ import kotlinx.coroutines.flow.update
 class MahasiswaViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(Mahasiswa())
     val uiState: StateFlow<Mahasiswa> = _uiState.asStateFlow()
+
+    fun saveDataMahasiswa(ls: MutableList<String>) {
+        _uiState.update { data ->
+            data.copy(
+                nama = ls[0],
+                gender = ls[1],
+                alamat = ls[2],
+                email = ls[3],
+                noHp = ls[4],  // Ensure correct list order for noHp
+                nim = ls[5]     // Ensure correct list order for nim
+            )
+        }
+    }
 }
+
