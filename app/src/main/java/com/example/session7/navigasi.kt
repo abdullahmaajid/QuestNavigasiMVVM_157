60a18f8515becdc63c383277d991c64a6da7e8e5
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-11-13 16:25:13 +0700

    di navigasi.kt menambahkan library yang diperlukan dan akan digunakan

--- app/src/main/java/com/example/session7/navigasi.kt
+++ app/src/main/java/com/example/session7/navigasi.kt
@@ -1,1 +1,18 @@
 package com.example.session7
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.session7.model.DataJK
+import com.example.session7.ui.view.FormulirView
+import com.example.session7.ui.view.TampilMahasiswaView
+import com.example.session7.ui.viewmodel.MahasiswaViewModel
