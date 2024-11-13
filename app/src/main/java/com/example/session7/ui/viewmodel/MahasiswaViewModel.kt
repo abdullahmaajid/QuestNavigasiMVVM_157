43decac590aebdcd99170721373e9d7f5f7bcc74
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-11-13 16:10:25 +0700

    di MahasiswaViewModel menambahkan class MahasiswaViewModel yang memanggil viewmodelnya

--- app/src/main/java/com/example/session7/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/session7/ui/viewmodel/MahasiswaViewModel.kt
@@ -7,3 +7,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
+class MahasiswaViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(Mahasiswa())
+    val uiState: StateFlow<Mahasiswa> = _uiState.asStateFlow()
+}
