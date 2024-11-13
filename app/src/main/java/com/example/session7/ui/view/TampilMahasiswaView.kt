39c15312ee35bd832740b0aa1e358b9118626c4c
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-11-13 16:20:35 +0700

    di TampilMahasiswaView.kt menambahkan library yang akan digunakan dengan sintaks import

--- app/src/main/java/com/example/session7/ui/view/TampilMahasiswaView.kt
+++ app/src/main/java/com/example/session7/ui/view/TampilMahasiswaView.kt
@@ -1,1 +1,11 @@
 package com.example.session7.ui.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.navigation.NavHostController
+import com.example.session7.model.Mahasiswa
