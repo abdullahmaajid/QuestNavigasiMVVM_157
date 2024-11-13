b33e5900841e0fd23d024b99b3cdbc2f585a0bd1
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-11-13 15:14:52 +0700

    di MahasiswaViewModel menambahkan library-library yang akan digunakan dengan sintaks import

---
+++ app/src/main/java/com/example/session7/ui/viewmodel/MahasiswaViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.session7.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.session7.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
