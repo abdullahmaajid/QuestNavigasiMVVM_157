83513fe198582dcdc15a869717e4f0bd4a08c4e6
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-11-13 15:12:19 +0700

    di Mahasiswa menambahkan class Mahasiswa serta variabel-variabel yang akan digunakan

--- app/src/main/java/com/example/session7/model/Mahasiswa.kt
+++ app/src/main/java/com/example/session7/model/Mahasiswa.kt
@@ -1,2 +1,10 @@
 package com.example.session7.model
 
+data class Mahasiswa(
+    val nama: String = "",
+    val gender: String = "",
+    val alamat: String = "",
+    val email: String = "",
+    val noHp: String = "",
+    val nim: String = ""
+)
